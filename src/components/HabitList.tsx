748ca6e03c41f81e02243b3ed79c1b0841258775
JGPico <minq_2@hotmail.com>
2026-05-11 14:38:12 -0600

    habit item component

---
+++ src/components/HabitList.tsx
@@ -0,0 +1,45 @@
+import Button from "./Button"
+
+export default function HabitList() {
+    const habits = [
+        { id: 1, name: "Lemon" },
+        { id: 2, name: "Banana" },
+        { id: 3, name: "Egg" }]
+
+    if (habits.length === 0) {
+        return <p className="text-center text-zinc-500 py-12">No habits yet. Add one above to get started</p>
+    }
+
+    return <div className="flex flex-col gap-3">
+        {habits.map(habit => {
+            return <HabitItem key={habit.id} habit={habit} />
+        })}
+    </div>
+}
+
+type HabitItemProps = {
+    habit: { id: string; name: string }
+}
+
+function HabitItem({ habit }: HabitItemProps) {
+    const visibleDates = [new Date()]
+    return (
+        <div className="rounded-x1 bg-zinc-800 p-4 flex flex-col gap-3">
+            <div className="flex items-center justify-between">
+                <div className=" flex gap-3 items-center">
+                    <span className="font-medium">{habit.name}</span>
+                    <span className="text-sm text-amber-400">3 🔥</span>
+                </div>
+                <Button>Delete</Button>
+            </div>
+            <div className="flex gap-1.5">
+                {visibleDates.map(date => {
+                    return <Button key={date.toISOString()}>
+                        <span className="font-medium">Mon</span>
+                        <span>2</span>
+                    </Button>
+                })}
+            </div>
+        </div>
+    )
+}
