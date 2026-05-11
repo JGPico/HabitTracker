81b83c79f652c87edfe098e7d4603f7717a4e359
JGPico <minq_2@hotmail.com>
2026-05-11 13:30:23 -0600

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,24 @@
+import Button from "./components/Button.tsx"
+
 export default function App() {
-  return null
+  return <div className="max-w-2x1 mx-auto p-4 flex flex-col gap-4">
+    <Header></Header>
+  </div>
+}
+
+function Header() {
+  return <header className="flex items-center justify-between">
+    <div className="flex flex-col gap-1">
+      <h1 className="text-3xl font-bold">Habit Tracker</h1>
+      <span className="text-zinc-400 text-sm">1 / 1 done today</span>
+    </div>
+
+    <div className="flex flex-col gap-1 items-end">
+      <span className="text-zinc-400 text-sm">Apr 6 - Apr 12</span>
+      <div className="flex items-center gap-3">
+        <Button >Prev</Button>
+        <Button >Next</Button>
+      </div>
+    </div>
+  </header>
 }
